c5b926a4b6a6be18981693d30df1af5570547f6a
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-11 13:44:03 +0200

    add collectionView

--- restaurant-app/OnboardingViewController.swift
+++ restaurant-app/OnboardingViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView : UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView(){
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        collectionView.collectionViewLayout = layout
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 
 }
 
+
+//MARK: - delegate and datasource extension
+
+extension OnboardingViewController: UICollectionViewDelegate,UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
+    
+    
+}
