2fefd4ae544a7368a1265cec567d8d1d77bd6eeb
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-11 16:14:21 +0200

    refactoring code

--- restaurant-app/OnboardingCell.swift
+++ restaurant-app/OnboardingCell.swift
@@ -6,41 +6,27 @@
 //
 
 import UIKit
-
-struct Slide {
-    let title: String
-    let animationName: String
-    let buttonColor: UIColor
-    let buttonTitle: String
-    
-    static let collection: [Slide] = [
-        .init(title: "Get your favourite food delivered to you under 30 minutes anytime", animationName: "lottieDeliveryGuy", buttonColor: .systemYellow, buttonTitle: "Next"),
-        .init(title: "We serve only from choiced restaurants in your area", animationName: "lottieRestaurant", buttonColor: .systemGreen, buttonTitle: "Order Now")
-    ]
-    
-}
-
-
+import Lottie
 
 class OnboardingCell: UICollectionViewCell {
     
-    @IBOutlet weak var animationView: UIView!
+    @IBOutlet weak var animationView: AnimationView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var actionButton: UIButton!
-    
-    
     @IBAction func actionButtonTapped(){
         
     }
-    
-    
     
     func configure(with slide: Slide){
         titleLabel.text = slide.title
         actionButton.backgroundColor = slide.buttonColor
         actionButton.setTitle(slide.buttonTitle, for: .normal)
         
+        let deliveryAnimation = Animation.named(slide.animationName)
+        animationView.animation = deliveryAnimation
+        animationView.loopMode = .loop
+        if !animationView.isAnimationPlaying{
+            animationView.play()
+        }
     }
-    
-    
 }
---
+++ restaurant-app/Slide.swift
@@ -0,0 +1,22 @@
+//
+//  Slide.swift
+//  restaurant-app
+//
+//  Created by Сергей Голенко on 11.01.2021.
+//
+
+import UIKit
+
+struct Slide {
+    let title: String
+    let animationName: String
+    let buttonColor: UIColor
+    let buttonTitle: String
+    
+    static let collection: [Slide] = [
+        .init(title: "Get your favourite food delivered to you under 30 minutes anytime", animationName: "lottieDelivery", buttonColor: .systemYellow, buttonTitle: "Next"),
+        .init(title: "We serve only from choiced restaurants in your area", animationName: "lottieRestaurant", buttonColor: .systemGreen, buttonTitle: "Order Now")
+    ]
+    
+}
+
