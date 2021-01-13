0e01da0ebaca077edb5f47fa3fc07302a634382d
SergeyGolenko <1989sergiotay@gmail.com>
2021-01-13 17:31:32 +0200

    go back to onBoardingVC when button is tapped

--- restaurant-app/PizzaIsDoneViewController.swift
+++ restaurant-app/PizzaIsDoneViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class PizzaIsDoneViewController: UIViewController {
-
+    
+    @IBAction func actionGoBackTapped(_ sender: Any) {
+        let onBoardingViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "onBoardingVC")
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let sceneDelegate = windowScene.delegate as? SceneDelegate,
+            let window = sceneDelegate.window {
+            window.rootViewController = onBoardingViewController
+            UIView.transition(with: window,
+                              duration: 3,
+                              options: .transitionCrossDissolve,
+                              animations: nil,
+                              completion: nil)
+        }
+    }
+        
+    @IBOutlet weak var actionGoBackButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        actionGoBackButton.layer.cornerRadius = 22
     }
     
 
